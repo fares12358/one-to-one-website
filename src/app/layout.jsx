a34ea1bf93b228562431fd1ba6e54b2ae6c894d5
fares12358 <fm883254@gmail.com>
2026-06-30 11:57:19 +0300

    updateing ui & adding new sections

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <div className="min-h-screen bg-white flex flex-col">
+        <div className="min-h-screen bg-white flex flex-col overflow-x-hidden">
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
